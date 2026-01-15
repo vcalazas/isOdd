8dadca415719bc6f67da71a5c09848173befe4b4
Vitor Gabriel Calazas Dalmolin <vitor.calazas@rrs-software.com.br>
2026-01-15 14:14:45 -0300

    the beginning of the most important project in the world

---
+++ index.js
@@ -0,0 +1,4 @@
+function isOdd(number) {
+    if (number === "odd" || number === "Odd" || number === "oDD" || number === "OdD" || number === "oDd" || number === "ODd" || number === "OdB" || number === "oDB" || number === "OdB" || number === "oDB") return true;
+}
+module.exports = isOdd;
